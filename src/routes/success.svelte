fbc18fbac943e92463db94ed736209c4e5fee705
Thomas Mol <email@thomasmol.com>
2022-07-13 16:56:22 +0200

    some changes

--- src/routes/success.svelte
+++ src/routes/success.svelte
@@ -12,9 +12,9 @@
 
 <div class="py-40 text-center">
 	<div class="p-4 ">
-		<h2 class="mb-12 text-2xl font-light text-gray-700">BizzFit</h2>
+		<h2 class="mb-12 text-2xl font-semibold text-zinc-700">BizzFit</h2>
 		<h1 class="mb-2 text-4xl font-semibold">Bedankt voor je bericht!</h1>
-		<p class="mb-8 text-xl text-gray-700">We zullen je zo snel mogelijk benaderen.</p>
+		<p class="mb-8 text-xl text-zinc-700">We zullen je zo snel mogelijk benaderen.</p>
 		<a href="/" class="font-bold text-yellow-600">Terug naar home</a>
 	</div>
 </div>
