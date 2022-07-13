e0770920b4a61aeb08f41f85fdd1a9ea53373e8e
Thomas Mol <email@thomasmol.com>
2022-07-13 17:10:32 +0200

    form fix

--- src/lib/components/EmailContactForm.svelte
+++ src/lib/components/EmailContactForm.svelte
@@ -1,5 +1,4 @@
 <form name="contact-form" method="POST" data-netlify="true" action="/success" class="space-y-5">
-	<input type="hidden" name="form-name" value="contact-form" />
 	<input
 		type="text"
 		name="name"
