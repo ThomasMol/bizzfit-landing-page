93f12bdefd932bea3dbb2a2255cf361bec970a94
basbergsma <basbergsma@ziggo.nl>
2022-07-25 16:44:35 +0200

    profielfoto's toegevoegd

--- src/lib/components/sections/Team.svelte
+++ src/lib/components/sections/Team.svelte
@@ -3,17 +3,17 @@
 		<h2 class="mb-10 py-10 text-center text-4xl font-semibold">Ons team</h2>
 		<div class="flex flex-wrap justify-evenly gap-4 text-center">
 			<div>
-				<img src="" alt="" class="h-24 w-24 rounded-lg bg-zinc-100" />
+				<img src="/images/bastian.jpg" alt="" class="h-24 w-24 rounded-lg bg-zinc-100" />
 				<h6 class="py-2 text-xl font-semibold">Bastian</h6>
 				<p class="text-orange-700">Business lead</p>
 			</div>
 			<div>
-				<img src="" alt="" class="h-24 w-24 rounded-lg bg-zinc-100" />
+				<img src="/images/thomas.jpg" alt="" class="h-24 w-24 rounded-lg bg-zinc-100" />
 				<h6 class="py-2 text-xl font-semibold">Thomas</h6>
 				<p class="text-orange-700">Tech lead</p>
 			</div>
 			<div>
-				<img src="" alt="" class="h-24 w-24 rounded-lg bg-zinc-100" />
+				<img src="/images/diederick.jpg" alt="" class="h-24 w-24 rounded-lg bg-zinc-100" />
 				<h6 class="py-2 text-xl font-semibold">Diederick</h6>
 				<p class="text-orange-700">Sales lead</p>
 			</div>
