2d7393309c22245d039fa9c418db0a12e8a56e42
Thomas Mol <email@thomasmol.com>
2023-01-03 20:21:57 +0100

    Added privacy policy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,113 @@
+<script lang="ts">
+</script>
+
+<div class="container py-20">
+	<h1 class="mt-2 text-xl font-semibold">Privacy Policy</h1>
+
+	<p class="mt-2">Last updated: 3 January 2023, Utrecht - The Netherlands</p>
+
+	<p class="mt-2">
+		Thank you for choosing our mobile fitness app Bizzfit ("App"). The App is designed to help you
+		track and improve your fitness and wellness, and we are committed to protecting your privacy
+		while you use it. This privacy policy ("Policy") describes the types of information we collect
+		when you use the App, how we use and share that information, and the choices you have with
+		respect to the information.
+	</p>
+
+	<p class="mt-2">
+		By downloading and using the App, you agree to the collection, use, and sharing of your
+		information as described in this Policy. If you do not agree with our policies and practices, do
+		not download or use the App.
+	</p>
+
+	<h2 class="mt-2 text-xl font-semibold">Information We Collect</h2>
+
+	<p class="mt-2">When you use the App, we may collect the following types of information:</p>
+
+	<ul class="mt-2 list-inside list-disc">
+		<li>
+			Information you import from other fitness apps: You may choose to import data from other
+			fitness apps into the App, such as your exercise history or fitness goals. This information is
+			collected and stored in the App, and is used to help you track and improve your fitness and
+			wellness.
+		</li>
+		<li>
+			Device information: We may collect information about your device, such as the type of device
+			you are using, the operating system, and the device's unique identifier.
+		</li>
+		<li>
+			Usage information: We may collect information about how you use the App, such as the features
+			you access and the actions you take.
+		</li>
+	</ul>
+
+	<h2 class="mt-2 text-xl font-semibold">How We Use Your Information</h2>
+
+	<p class="mt-2">
+		We use the information we collect to provide and improve the App, and to personalize your
+		experience. Specifically, we may use your information to:
+	</p>
+
+	<ul class="mt-2 list-inside list-disc">
+		<li>
+			Provide and maintain the App: We use the information we collect to provide and maintain the
+			App, and to improve its performance and functionality.
+		</li>
+		<li>
+			Personalize your experience: We use the information we collect to personalize your experience
+			within the App, such as by suggesting exercises or goals based on your previous activity.
+		</li>
+		<li>
+			Analyze and improve the App: We may use the information we collect to analyze and understand
+			how the App is used, and to improve it based on that analysis.
+		</li>
+	</ul>
+
+	<h2 class="mt-2 text-xl font-semibold">Sharing Your Information</h2>
+
+	<p class="mt-2">
+		We do not share your personal information with third parties, except in the limited
+		circumstances described below:
+	</p>
+
+	<ul class="mt-2 list-inside list-disc">
+		<li>
+			With your consent: We may share your information with third parties if you give us your
+			explicit consent to do so.
+		</li>
+		<li>
+			To provide the App: We may share your information with third parties who provide services on
+			our behalf, such as hosting the App or analyzing data. These third parties are required to use
+			your information only for the purpose of providing the requested service, and are not allowed
+			to share or use it for any other purpose.
+		</li>
+		<li>
+			As required by law: We may be required to disclose your information to third parties if we are
+			required to do so by law, such as in response to a subpoena or court order.
+		</li>
+	</ul>
+
+	<h2 class="mt-2 text-xl font-semibold">Your Choices</h2>
+
+	<p class="mt-2">You have the following choices with respect to your information:</p>
+
+	<ul class="mt-2 list-inside list-disc">
+		<li>
+			Importing data from other fitness apps: You may choose not to import data from other fitness
+			apps into the App. If you do choose to import data, you can control the information that is
+			imported by selecting the specific data points you want to import.
+		</li>
+		<li>
+			Device permissions: You may be asked to grant the App certain permissions on your device, such
+			as access to your camera or location. You can control these permissions through your device's
+			settings, and may choose to revoke them at any time.
+		</li>
+		<li>
+			<!-- Opting out of analysis: We may use tools such as cookies and analytics software to analyze and
+			understand how the App is used. If you do not want us to use these tools to collect and
+			analyze information about your use of the App, you can opt out by adjusting the settings on
+			your device or in the App. Please note that opting out may affect the performance and
+			functionality of the App. -->
+		</li>
+	</ul>
+</div>
